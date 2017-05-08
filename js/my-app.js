bd12553d3bb659628251fe36b8fc7d40a3c17531
zhanzhang2014 <zhan.hang.2013@gmail.com>
2017-05-08 09:33:25 +0800

    add sites

--- js/my-app.js
+++ js/my-app.js
@@ -3,6 +3,7 @@ Template7.registerHelper('json_stringify', function (context) {
     return JSON.stringify(context);
 });
 
+var xlsFile = {};
 // Initialize your app
 var myApp = new Framework7({
     //Disable App's automatica initialization
@@ -17,23 +18,39 @@ var myApp = new Framework7({
     // Specify Template7 data for pages
     template7Data: {
         // cable crane data
-        cable_crane_1_1:{},
-        cable_crane_1_2:{},
-        cable_crane_1_3:{},
-        cable_crane_1_4:{},
-        cable_crane_2_1:{},
-        cable_crane_2_2:{},
-        cable_crane_2_3:{},
-        cable_crane_2_4:{},
-        cable_crane_2_4:{},
-        cable_crane_3_1:{},
-        cable_crane_3_2:{},
-        cable_crane_3_3:{},
-        cable_crane_3_4:{},
-        cable_crane_4_1:{},
-        cable_crane_4_2:{},
-        cable_crane_4_3:{},
-        cable_crane_4_4:{}
+        cable_crane_1_1:{
+            Sheet1:[{}]
+        },
+        cable_crane_2_1:{
+            Sheet1:[{}]
+        },
+        cable_crane_2_2:{
+            Sheet1:[{}]
+        },
+        cable_crane_3_1:{
+            Sheet1:[{}]
+        },
+        cable_crane_3_2:{
+            Sheet1:[{}]
+        },
+        cable_crane_4_1:{
+            Sheet1:[{}]
+        },
+        cable_crane_4_2:{
+            Sheet1:[{}]
+        },
+        cable_crane_5_1:{
+            Sheet1:[{}]
+        },
+        cable_crane_5_2:{
+            Sheet1:[{}]
+        },
+        cable_crane_6_1:{
+            Sheet1:[{}]
+        },
+        cable_crane_6_2:{
+            Sheet1:[{}]
+        }
     }
 });
 
@@ -43,21 +60,17 @@ myApp.onPageInit('index-1', function (page) {
   //startRefreashData();
   console.log("Initialize main page and data");
   loadXMLDoc("LJ0_OPC_WINCC","cable_crane_1_1");
-  loadXMLDoc("LJ1_OPC_WINCC","cable_crane_1_2");
-  loadXMLDoc("LJ2_OPC_WINCC","cable_crane_1_3");
-  loadXMLDoc("LJ3_OPC_WINCC","cable_crane_1_4");
-  loadXMLDoc("LJ4_OPC_WINCC","cable_crane_2_1");
-  loadXMLDoc("LJ5_OPC_WINCC","cable_crane_2_2");
-  loadXMLDoc("LJ6_OPC_WINCC","cable_crane_2_3");
-  loadXMLDoc("LJ7_OPC_WINCC","cable_crane_2_4");
-  loadXMLDoc("LJ8_OPC_WINCC","cable_crane_3_1");
-  loadXMLDoc("LJ9_OPC_WINCC","cable_crane_3_2");
-  loadXMLDoc("LJ10_OPC_WINCC","cable_crane_3_3");
-  loadXMLDoc("LJ11_OPC_WINCC","cable_crane_3_4");
-  loadXMLDoc("LJ12_OPC_WINCC","cable_crane_4_1");
-  loadXMLDoc("LJ13_OPC_WINCC","cable_crane_4_2");
-  loadXMLDoc("LJ14_OPC_WINCC","cable_crane_4_3");
-  loadXMLDoc("LJ15_OPC_WINCC","cable_crane_4_4");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_2_1");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_2_2");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_3_1");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_3_2");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_4_1");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_4_2");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_5_1");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_5_2");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_6_1");
+  loadXMLDoc("LJ0_OPC_WINCC","cable_crane_6_2");
+
 });
 
 //maniually initialize home page to activate the function above
@@ -78,6 +91,11 @@ var refreashState = true;
 var refreashTimer;
 
 function specifyCrane(datafile,crane){
+    cur_datafile = datafile;
+    cur_crane = crane;
+}
+
+function handleCraneData(crane){
     cur_datafile = datafile;
     cur_crane = crane;
 }
@@ -147,14 +165,26 @@ function loadXMLDoc(datafile,crane)
             var arr = new Array();
             for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
             var wb = XLSX.read(arr.join(""), {type:"binary"});
-            myApp.template7Data[crane]=process_wb(wb);
+            //myApp.template7Data[crane]=process_wb(wb);
+            xlsFile = process_wb(wb);
+            siteName = crane.split("_")[2];
+            var craneNum = (parseInt(crane.split("_")[3])-1)*14;
+            for( var i=0;i<14;i++){
+                myApp.template7Data[crane].Sheet1[i] = {"NAME":xlsFile.Sheet1[i+craneNum]["NAME"+siteName],"DATA":xlsFile.Sheet1[i+craneNum]["DATA"+siteName]};
+            }
         };
     } else {
         xmlhttp.setRequestHeader("Accept-Charset", "x-user-defined");  
         xmlhttp.onreadystatechange = function() { if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var ff = convertResponseBodyToText(xmlhttp.responseBody);
             var wb = XLSX.read(ff, {type:"binary"});
-            myApp.template7Data[crane]=process_wb(wb);
+            //myApp.template7Data[crane]=process_wb(wb);
+            xlsFile = process_wb(wb);
+            siteName = crane.split("_")[2];
+            var craneNum = (parseInt(crane.split("_")[3])-1)*14;
+            for( var i=0;i<14;i++){
+                myApp.template7Data[crane].Sheet1[i] = {"NAME":xlsFile.Sheet1[i+craneNum]["NAME"+siteName],"DATA":xlsFile.Sheet1[i+craneNum]["DATA"+siteName]};
+            }
         } };
     }
     xmlhttp.send();
